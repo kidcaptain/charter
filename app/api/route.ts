c08b823cef53ad11629ac9eec83cc39c33d8143f
hunterjul007 <93106837+hunterjul007@users.noreply.github.com>
2024-02-25 20:39:14 +0100

    first commit

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "./auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/authOptions";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
