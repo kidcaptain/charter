7c12e712316aee3a8c3537635b78771389792706
kidcaptain <113309370+kidcaptain@users.noreply.github.com>
2024-02-25 22:24:17 +0100

    Update authOptions.tsx

--- app/lib/authOptions.tsx
+++ app/lib/authOptions.tsx
@@ -6,7 +6,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 
  export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXTAUTH_SECRET,
+ 
     session: {
         strategy: "jwt"
     },
