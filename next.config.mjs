5748d4d3cf060f539bbde630a8e1ff86625756de
hunterjul007 <93106837+hunterjul007@users.noreply.github.com>
2024-03-18 22:39:07 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,34 @@
 /** @type {import('next').NextConfig} */
 import withPWAInit from "@ducanh2912/next-pwa";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
 
-const withPWA = withPWAInit({
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    dest: "public",
-    fallbacks: {
-      //image: "/static/images/fallback.png",
-      document: "/offline", // if you want to fallback to a custom page rather than /_offline
-      // font: '/static/font/fallback.woff2',
-      // audio: ...,
-      // video: ...,
-    },
-    workboxOptions: {
-      disableDevLogs: true,
-    },
-})
-
-const nextConfig = {
-     
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+        cacheOnFrontEndNav: true,
+        aggressiveFrontEndNavCaching: true,
+        reloadOnOnline: true,
+        swcMinify: true,
+        dest: "public",
+        fallbacks: {
+          //image: "/static/images/fallback.png",
+          document: "/offline", // if you want to fallback to a custom page rather than /_offline
+          // font: '/static/font/fallback.woff2',
+          // audio: ...,
+          // video: ...,
+        },
+        workboxOptions: {
+          disableDevLogs: true,
+        },
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
 };
 
-export default withPWA(nextConfig);
+const nextConfig = {};
+
+export default nextConfigFunction;
